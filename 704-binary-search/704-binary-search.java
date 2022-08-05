25ae6148058e5298976b42f32694521390bc3d01
Yunkeun <70425484+Yunkeun@users.noreply.github.com>
2022-08-05 16:45:28 +0900

    Time: 0 ms (100.00%), Space: 53.6 MB (80.07%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = (start + end) / 2;
+        while (end >= start) {
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return -1;
+    }
+}
